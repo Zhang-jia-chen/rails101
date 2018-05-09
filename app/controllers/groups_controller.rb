d0f3b309917abed87ac5fbc824aeae64470cd624
Zhang-jia-chen <zjc834723074@gmail.com>
2018-05-09 18:48:25 +0800

    when created a group, join it automatically

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,6 +1,32 @@
 class GroupsController < ApplicationController
-  before_action :authenticate_user! , only: [:new,:create, :edit, :update, :destroy]
+  before_action :authenticate_user! , only: [:new,:create, :edit, :update, :destroy,:join, :quit]
   before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
+
+  def join
+    @group = Group.find(params[:id])
+
+    if !current_user.is_member_of?(@group)
+      current_user.join!(@group)
+      flash[:notice] = "加入讨论版成功"
+    else
+      flash[:warning] = "你已经是本讨论版成员了!"
+    end
+
+    redirect_to group_path(@group)
+  end
+
+  def quit
+    @group = Group.find(params[:id])
+
+    if current_user.is_member_of?(@group)
+      current_user.quit!(@group)
+      flash[:notice] = "退出讨论版成功"
+    else
+      flash[:warning] = "你不是讨论版成员"
+    end
+
+    redirect_to group_path(@group)
+  end
 
   def index
     @groups = Group.all
@@ -23,7 +49,7 @@ class GroupsController < ApplicationController
     @group.user = current_user
 
     if @group.save
-
+      current_user.join!(@group)
       redirect_to groups_path
     else
       render 'new'
